3f262d32d83d36c052b1a4d9ea09d95a43925302
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-07-23 19:55:38 +0000

    created tables and their relationships.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_144903) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_23_194315) do
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "note_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["note_id"], name: "index_favorites_on_note_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.integer "topic_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["topic_id"], name: "index_notes_on_topic_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +49,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_144903) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "notes"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "notes", "topics"
+  add_foreign_key "notes", "users"
 end
