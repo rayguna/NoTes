f0f852885acce3e69804d7b2c4df20ca2da00449
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-07-23 21:09:34 +0000

    added a new field called username to the user table, as well as to the user sign-up form.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_194315) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_23_204905) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "note_id", null: false
@@ -45,8 +45,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_194315) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "favorites", "notes"
