2192614e9b5791f98f2f56b67b49bdd4e5a4ebcf
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-07-24 15:20:19 +0000

    Executed the scaffold_controller command to generate the RCAV patterns for all tables.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -65,6 +65,6 @@ class NotesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def note_params
-      params.require(:note).permit(:title, :content, :user_id)
+      params.require(:note).permit(:title, :content, :user_id, :topic_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :favorites
+  resources :users
+  resources :topics
   resources :notes
   devise_for :users
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
