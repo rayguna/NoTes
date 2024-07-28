2ccddf2d4d7cba690e7c3ede122892c12991435e
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-07-28 15:30:18 +0000

    Worked on navigate page, which includes adding a link to tools page and renaming home.html.erb to navigate.html.erb.

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,9 @@ Rails.application.routes.draw do
     end
   end
 
-  root "pages#home"
+  get 'tools', to: 'pages#tools'
+
+  root "pages#navigate"
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get "/your_first_screen" => "pages#first"
