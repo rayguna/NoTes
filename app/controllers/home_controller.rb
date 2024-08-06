7261329095e4b4a7d15c5dcacb7d11624c067ce0
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-08-06 15:32:04 +0000

    Got the signed in page to work as intended with the javascript plot being functional.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,31 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-  # skip_after_action :verify_authorized
 
   def index
-    
+    if user_signed_in?
+      prepare_navigation_data
+    end
   end
 
   def about
   end
 
+  private
+
+  def prepare_navigation_data
+    @months = (1..12).map { |m| Date::MONTHNAMES[m] }
+    selected_year = params[:year] ? params[:year].to_i : Date.today.year
+
+    if params[:show_chart] == "true"
+      @topics_data = (1..12).map do |month|
+        Topic.where(user_id: current_user.id, created_at: Date.new(selected_year, month).all_month).count
+      end
+      @notes_data = (1..12).map do |month|
+        Note.where(user_id: current_user.id, created_at: Date.new(selected_year, month).all_month).count
+      end
+    else
+      @topics_data = []
+      @notes_data = []
+    end
+  end
 end
