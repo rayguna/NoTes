1a611e4d2244f87801cc18cb4bae126478a9f7ee
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-08-06 15:22:43 +0000

    set navigate as a partial within the home#index page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,7 @@ class HomeController < ApplicationController
   # skip_after_action :verify_authorized
 
   def index
-    if user_signed_in? 
-      redirect_to :navigate
-    else
-      render :index
-    end
+    
   end
 
   def about
