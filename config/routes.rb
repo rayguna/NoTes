578417c3cb43e05232041ff8b1508f82ceeabf7a
rayguna <ray_guna@yahoo.com>
2024-08-12 17:10:35 -0500

    cleaned up routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,31 @@
 Rails.application.routes.draw do
-  resources :follow_requests
   devise_for :users
 
-  resources :favorites
-  resources :users
-  resources :notes
-
-  resources :teases
-
-  resources :topics do
-    resources :notes
+  resources :users do
+    get 'friends', on: :collection
   end
 
+  resources :favorites
   resources :notes do
     collection do
       post :preview
     end
   end
-  
-  get 'friends', to: 'users#friends' 
-  
 
-  get 'tools', to: 'pages#tools'
+  resources :teases
 
-  get "/navigate" => "pages#navigate", as: :navigate
-  
-  root "home#index"
+  resources :topics do
+    resources :notes, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  end
 
-  resources :follow_requests
-
-  # config/routes.rb
   resources :follow_requests do
     member do
       patch :accept
     end
   end
 
+  get 'tools', to: 'pages#tools'
+  get 'navigate', to: 'pages#navigate', as: :navigate
+  
+  root "home#index"
 end
