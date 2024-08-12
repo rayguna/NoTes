c40cd64496376eb1e246cc6664734154f4dd6e86
rayguna <ray_guna@yahoo.com>
2024-08-12 16:42:29 -0500

    tried to rename follow_request to friend request table, but encountered error, so I reverted the table name back to follow_request table. when reverting, the error was that the follow_request table is not found. I had to remake the follow_request table to get it to work.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_12_135829) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_12_214005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -55,9 +55,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_12_135829) do
   create_table "follow_requests", force: :cascade do |t|
     t.bigint "recipient_id", null: false
     t.bigint "sender_id", null: false
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.index ["recipient_id"], name: "index_follow_requests_on_recipient_id"
     t.index ["sender_id"], name: "index_follow_requests_on_sender_id"
   end
