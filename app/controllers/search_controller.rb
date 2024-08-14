5d04d97d9a449c012ae74b33e8fe494285454cff
rayguna <ray_guna@yahoo.com>
2024-08-14 11:47:21 -0500

    added constrain to perform search limited to the current user.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
-# app/controllers/search_controller.rb
 class SearchController < ApplicationController
+  before_action :set_current_user
+
   def index
     @search_results = { notes: [], topics: [] }
 
@@ -7,7 +8,7 @@ class SearchController < ApplicationController
     case_sensitive = params[:match_case] == '1' # Assuming '1' indicates match case enabled
 
     if search_words.present?
-      decrypted_notes = Note.all.select do |note|
+      decrypted_notes = current_user.notes.select do |note|
         decrypted_title = decrypt_and_search(note, :title, search_words, case_sensitive)
         decrypted_content = decrypt_and_search(note, :content, search_words, case_sensitive)
         decrypted_topic_name = decrypt_and_search(note.topic, :name, search_words, case_sensitive)
@@ -15,7 +16,7 @@ class SearchController < ApplicationController
         decrypted_title || decrypted_content || decrypted_topic_name
       end
 
-      decrypted_topics = Topic.all.select do |topic|
+      decrypted_topics = current_user.topics.select do |topic|
         decrypted_name = decrypt_and_search(topic, :name, search_words, case_sensitive)
         decrypted_name
       end
@@ -24,7 +25,7 @@ class SearchController < ApplicationController
       @search_results[:topics] = decrypted_topics
     else
       if params[:topic_name_cont].present?
-        decrypted_topics = Topic.all.select do |topic|
+        decrypted_topics = current_user.topics.select do |topic|
           decrypted_name = decrypt_and_search(topic, :name, [params[:topic_name_cont]], case_sensitive)
           decrypted_name
         end
@@ -32,7 +33,7 @@ class SearchController < ApplicationController
       end
 
       if params[:content_cont].present?
-        decrypted_notes = Note.all.select do |note|
+        decrypted_notes = current_user.notes.select do |note|
           decrypted_content = decrypt_and_search(note, :content, [params[:content_cont]], case_sensitive)
           decrypted_content
         end
@@ -42,6 +43,10 @@ class SearchController < ApplicationController
   end
 
   private
+
+  def set_current_user
+    @current_user = current_user
+  end
 
   def decrypt_and_search(record, attribute, words, case_sensitive)
     decrypted_text = record.decrypt(attribute)
