4293141e436786db49d6412bed441a1114a63685
Ray Gunawidjaja <33289654+rayguna@users.noreply.github.com>
2024-08-20 18:44:25 +0000

    fixed delete table error due to dependency with facvorite table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_20_134935) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_20_184139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_20_134935) do
     t.bigint "topic_id"
     t.string "favoritable_type"
     t.bigint "favoritable_id"
+    t.integer "note_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
